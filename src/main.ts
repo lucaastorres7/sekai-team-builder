f635419becdb8cad8e6edf74963aa6723f52d97e
Lucas Torres <lucastorreslr10@gmail.com>
2025-07-05 21:36:39 -0300

    feat: added cors origin to test integration

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
